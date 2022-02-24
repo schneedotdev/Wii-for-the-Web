23ef85deeac1cb8aa5719b20cf884af7cf6fbe63
Brian Schnee <brian.a.schnee@gmail.com>
2022-02-24 06:07:19 -0500

    refactoring event listener. Split actions into multiple functions. Need to build responsive movement with the controller. Limited controller range

--- js/main.js
+++ js/main.js
@@ -1,40 +1,67 @@
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
 document.addEventListener('mousemove', (e) => {
     let x = e.pageX;
     let y = e.pageY;
-    console.log(x,y);
-    
-    // document.querySelector('.controller').style.margin = `${y}px 0 0 ${x}px`;
-    // document.querySelector('.controller').style.transform = `translate(${x}px, ${y}px)`;
+    console.log(`x: ${x}`,`y: ${y}`);
 
-    document.querySelector('.controller').style.left = `${x}px`;
-    document.querySelector('.controller').style.top = `${y}px`;
-
-    // if (y > 450) {
-    //     document.querySelector('.controller').style.height = "50rem";
-    // } else if (y > 370) {
-    //     document.querySelector('.controller').style.height = "40rem";
-    // } else if (y > 300) {
-    //     document.querySelector('.controller').style.height = "30rem";
-    // } else {
-    //     document.querySelector('.controller').style.height = "20rem";
-    // }
-
-    // if (x > 600) {
-    //     document.querySelector('.controller').style.transform = "rotate(30deg)";
-    // } else if (x > 500) {
-    //     document.querySelector('.controller').style.transform = "rotate(20deg)";
-    // } else if (x > 400) {
-    //     document.querySelector('.controller').style.transform = "rotate(10deg)";
-    // } else if (x > 300) {
-    //     document.querySelector('.controller').style.transform = "rotate(0deg)";
-    // } else if (x > 200) {
-    //     document.querySelector('.controller').style.transform = "rotate(-10deg)";
-    // } else if (x > 100) {
-    //     document.querySelector('.controller').style.transform = "rotate(-20deg)";
-    // } else {
-    //     document.querySelector('.controller').style.transform = "rotate(-30deg)";
-    // }
-
+    moveImage(x, y);
+    tiltImage(y);
+    rotateImage(x);
 });
 
-console.log('output');
+
+/**
+ * name: moveImage
+ * description: moves the image element with a class of controller on the DOM based on the position of your mouse cursor
+ * @param x: x-axis value of the cursor in reference to the window width
+ * @param y: y-axis value of the cursor in reference to the window height
+ */
+function moveImage(x, y) {
+    if (x >= windowWidth * 0.3 && x <= windowWidth * 0.7) {
+        document.querySelector('.controller').style.left = `${x}px`;
+    } else if (x < windowWidth * 0.3){
+        document.querySelector('.controller').style.left = `${windowWidth * 0.3}px`;
+    } else {
+        document.querySelector('.controller').style.left = `${windowWidth * 0.7}px`;
+    }
+
+    document.querySelector('.controller').style.top = `${y}px`;
+}
+
+/**
+ * name: tiltImage
+ * description: changes the height of the controller element to simulate tilting of a controller
+ * @param y: y-axis value of the cursor in reference to the window height
+ */
+function tiltImage(y) {
+    // let twentiethOfScreenHeight = Math.round(window.innerHeight / 20);
+
+    // if (y < twentiethOfScreenHeight * 5) {
+    //     document.querySelector('.controller').style.height = "35rem";
+    // } else if (y < twentiethOfScreenHeight * 6) {
+    //     document.querySelector('.controller').style.height = "40rem";
+    // } else if (y < twentiethOfScreenHeight * 7) {
+    //     document.querySelector('.controller').style.height = "45rem";
+    // } else if (y < twentiethOfScreenHeight * 8) {
+    //     document.querySelector('.controller').style.height = "50rem";
+    // } else if (y < twentiethOfScreenHeight * 9) {
+    //     document.querySelector('.controller').style.height = "55rem";
+    // } else {
+    //     document.querySelector('.controller').style.height = "60rem";
+    // }
+}
+
+/**
+ * name: rotateImage
+ * description: rotates the image element with a class of controller based on the value of the cursors horizontal positioning. To simmulate pointing a controller left and right.
+ * @param x: x-axis value of the cursor in reference to the window width
+ */
+function rotateImage(x) {
+    let arcscale = Math.round((x - (windowWidth / 2)) / 15);
+    // let imageOffset = Math.round((x / windowWidth));
+    
+    document.querySelector('.controller').style.rotate = `${arcscale}deg`;
+    // document.querySelector('.controller').style.transform = `${imageOffset}%`;
+}
